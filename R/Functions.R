deea79cd2b329d625e80caaedb98dc18e7ee07bc
hstojic <hstojic@yahoo.com>
2015-05-04 13:14:19 +0200

    Moved changeClass function to file other.R, to have them in better logical sections.

--- R/Functions.R
+++ R/Functions.R
@@ -1,49 +1,4 @@
 # ----------------------------------------------------------------------
-# Change class of multiple variables
-# ----------------------------------------------------------------------
-#' Change class of multiple variables in one go. 
-#' 
-#' Changing classes of many variables in the data frame simultaneously. The function also checks if one is converting a factor to numeric variable and it will make a correct
-#' conversion without loss of digits.
-#'
-#' @param df A data frame.
-#' @param newClass A character vector which specifies desired class of each
-#' variable in \code{df}.
-#' @return The data frame \code{df} where each column has a class as specified 
-#' in \code{newClass}.
-#' @seealso \code{\link{as.numeric}}, \code{\link{as.character}}, \code{\link{as.factor}} which this function wraps
-#' @export
-#' @examples
-#' # check the classes of variables in mtcars dataset
-#' sapply(mtcars, class)
-#' # change them to something else
-#' changeClass(mtcars, c("fac", rep("char",10)))
-
-
-changeClass <- function(df, newClass) {
-    # assert that input is of appropriate format
-    assert_that(is.data.frame(df), dim(df)[2]==length(newClass), is.character(newClass))
-    if (!all(newClass %in% c("num", "char", "fac"))) stop("One or more of new classes is not defined appropriately as either 'num', 'char' or 'fac'.")
-
-    for(i in 1:length(newClass)) { 
-        if (newClass[i] == "num") { 
-            if(is.factor(df[,i])) {
-                df[,i] <- as.numeric(levels(df[,i]))[df[,i]]
-            } else {
-                df[,i] <- as.numeric(df[,i])
-            }
-        } else if (newClass[i] == "char") {
-            df[,i] <- as.character(df[,i])
-        } else if (newClass[i] == "fac") {
-            df[,i] <- as.factor(df[,i])
-        }       
-    }
-    return(df)
-}
-
-
-
-# ----------------------------------------------------------------------
 # Rep for matrices
 # ----------------------------------------------------------------------
 #' Replicate matrices.  
@@ -112,10 +67,3 @@ repvec<-function(vec, nrep, dim){
 }
 
 
-
-# setwd("/home/hrvoje/Dropbox/Research/Code/R/")
-# setwd("/home/hrvoje/Dropbox/Research/Code/R/hfunk/")
-# getwd()
-# devtools::check("hfunk")
-# library(hfunk)
-# devtools::install_github("hstojic/hfunk")
