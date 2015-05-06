c1bcf9aea87bfdd5cd8121657a830ca3a5f3b2d6
hstojic <hstojic@yahoo.com>
2015-05-06 13:04:39 +0200

    Corrected some mod tests and added a test for inputs that should throw an error.

--- tests/testthat/test_other.R
+++ tests/testthat/test_other.R
@@ -23,8 +23,8 @@ test_that("mod w/o freq - numeric vector, no ties", {
     res <- mod(x)
 
     expect_that( res, is_identical_to(1) )
-    expect_that( res, is_a("numerical") )
-    expect_that( length(res), is_identical_to(1) )
+    expect_that( res, is_a("numeric") )
+    expect_that( length(res), is_identical_to(1L) )
 })
 
 
@@ -35,8 +35,8 @@ test_that("mod w/o freq - numeric vector, no ties, with NA's", {
     res <- mod(x)
 
     expect_that( res, is_identical_to(1) )
-    expect_that( res, is_a("numerical") )
-    expect_that( length(res), is_identical_to(1) )
+    expect_that( res, is_a("numeric") )
+    expect_that( length(res), is_identical_to(1L) )
 })
 
 
@@ -47,8 +47,8 @@ test_that("mod w/o freq - numeric vector, with ties", {
     res <- mod(x)
 
     expect_that( res, is_identical_to(c(1,2)) )
-    expect_that( res, is_a("numerical") )
-    expect_that( length(res), is_identical_to(2) )
+    expect_that( res, is_a("numeric") )
+    expect_that( length(res), is_identical_to(2L) )
 })
 
 test_that("mod w freq - numeric vector, with ties", {
@@ -59,10 +59,20 @@ test_that("mod w freq - numeric vector, with ties", {
 
     expect_that( res$mod, is_identical_to(c(1,2)) )
     expect_that( res, is_a("list") )
-    expect_that( res$mod, is_a("numerical") )
-    expect_that( res$freq, is_a("numerical") )
-    expect_that( length(res$mod), is_identical_to(2) )
-    expect_that( length(res$freq), is_identical_to(1) )
+    expect_that( res$mod, is_a("numeric") )
+    expect_that( res$freq, is_a("integer") )
+    expect_that( length(res$mod), is_identical_to(2L) )
+    expect_that( length(res$freq), is_identical_to(1L) )
+})
+
+test_that("mod - inputs that should throw error", {
+
+    # some inputs that should fail
+    expect_error( mod() )
+    expect_error( mod(c(1,2,2), NA) )
+    expect_error( mod(list(1,2,2)) )
+    expect_error( mod(matrix(1,2,2)) )
+    expect_error( mod(data.frame(1,2,2)) )
 })
 
 
