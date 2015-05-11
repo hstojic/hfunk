7c9dcea3080a9cdfc164806c6595883364efb505
Hrvoje <hstojic@yahoo.com>
2015-05-11 12:51:30 +0200

    Moved mod function to stats.R

--- R/other.R
+++ R/other.R
@@ -1,65 +1,4 @@
 # ----------------------------------------------------------------------
-# Mod function. 
-# ----------------------------------------------------------------------
-#' Computes a mod statistic. 
-#' 
-#' A function for finding the most frequent element in a vector and optionally its frequency. The intended use is not for estimating mod statistic, but for finding a mod in sequences where elements repeat themselves often. Hence, it should not be used with vectors of real numbers, as such elements are likely to be unique. Note that the function is not capable of breaking ties randomly.
-#'
-#' @param x A numeric, logical or a character vector where the mod needs to be found.
-#' @param freq A logical that indicates whether frequency of elements in the mod should be returned as well. By default set to FALSE.
-#' @param ties A string that indicates how the ties should be broken. Possible values are: "unbroken" in which case ties are not broken and all of the tied elements are returned in the output (in this case, if \code{freq} is set to TRUE, frequency output will be a scalar that indicates frequency of all tied elements), and "first" where only the first element is returned. By default set to "unbroken".
-#' @param na.rm A logical that indicates whether NA elements should be removed or not. By default set to TRUE.
-#' @return If \code{freq} set to FALSE, the output is a vector of the same class as \code{x} with a single element, indicating the mod of the vector provided in \code{x}. If \code{freq} set to FALSE, the output is a list with first element being the mod and second element being the frequency of the mod.
-#' @import assertthat
-#' @export
-#' @examples
-#' # create some numeric vectors
-#' x <- c(1,1,1,2,2)
-#' y <- c(1,1,2,2)
-#' 
-#' # no ties, with and without reporting frequency
-#' mod(x)
-#' mod(x, freq = TRUE)
-#'
-#' # ties, with and without reporting frequency
-#' mod(y)
-#' mod(y, freq = TRUE)
-
-mod <- function(x, freq = FALSE, ties = "unbroken", na.rm = TRUE) {
-    # basic checks of the inputs
-    not_empty(x)
-    assert_that(is.logical(freq))
-    assert_that(is.logical(na.rm))
-    assert_that(is.character(ties))
-    assert_that(  # a num/char/logical vector
-        !is.matrix(x),
-        !is.list(x),
-        !is.data.frame(x)
-    )
-    
-    # extract a list of unique elements in x 
-    if (na.rm) x <- x[!is.na(x)]
-    ux <- unique(x)
-    tab <- tabulate(match(x, ux))
-
-    # find which is the most frequent, depending on type of breaking ties
-    if (ties == "unbroken") {
-        mod <- ux[tab == max(tab)]
-    } else if (ties == "first") {
-        mod <- ux[which.max(tab)]
-    }
-    
-    # returning the mod, if freq=TRUE then also with number of elements in mod
-    if (freq) {
-        modFreq <- max(tab)
-        return(list(mod = mod, freq = modFreq))
-    } else {
-        return(mod)
-    }
-}
-
-
-# ----------------------------------------------------------------------
 # A shortcut for evaluating a string. 
 # ----------------------------------------------------------------------
 #' A shortcut for evaluating an expression in a string format. 
@@ -87,9 +26,9 @@ streval <- function(x) {return(eval(parse(text=x)))}
 # ----------------------------------------------------------------------
 # A shortcut for negation of the %in% operator. 
 # ----------------------------------------------------------------------
-#' A shortcut for negation of the %in% operator. 
+#' A shortcut for negation of the \%in\% operator. 
 #' 
-#' Using a negation of %in% operator in the standard format produces less readable code. More natural way of using negation in this context would be to prepend 'in' with standard negation symbol, '!', which then results in using shorter and more readable 'x %!in% y' instead of '!(x %in% y)'.
+#' Using a negation of \%in\% operator in the standard format produces less readable code. More natural way of using negation in this context would be to prepend 'in' with standard negation symbol, '!', which then results in using shorter and more readable 'x \%!in\% y' instead of '!(x \%in\% y)'.
 #'
 #' @param x A data structure whose elements will be checked whether they are also present in data structure \code{y}.
 #' @param y A data structure that will be checked whether elements from \code{x} are present in it.
