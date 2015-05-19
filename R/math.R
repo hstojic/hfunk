83e68783ff630ad8b7df0dda5e2f84be067e7926
Hrvoje <hstojic@yahoo.com>
2015-05-19 13:23:16 +0200

    Improved version fo sqrtmat(), added convergence criterion and some basic tests.

--- R/math.R
+++ R/math.R
@@ -3,49 +3,58 @@
 # ----------------------------------------------------------------------
 #' Computes square root of a matrix. 
 #' 
-#' A function for rescaling a vector of integers or real numbers to an arbitrary interval. Element in the original vector with the minimum value will be transformed to a number deteremined by  \code{lower} argument (zero by default), while the element with the maximum value will be transformed to  a number deteremined by \code{upper} argument (one by default). All other elements will be rescaled proportionally.
+#' A function for computing the square roots of a quadratic matrix using the Denman-Beavers algorithm. note that convergence is not guaranteed, even for matrices that do have square roots.
 #'
-#' @param x A numeric vector where numbers need to be rescaled.
-#' @param lower Desired minimum value, by deafult set to 0.
-#' @param upper Desired maximum value, by deafult set to 1.
-#' @param na.rm A logical that indicates whether NA elements should be removed from the output or not. By default set to FALSE. Note that, if it is set to FALSE, the rescale will still remove NA's to do the computations, but it will simply leave the NA elements in the resulting vector.
-#' @return A numeric vector with rescaled elements from \code{x}.
+#' @param mat A numeric matrix where numbers need to be rescaled.
+#' @param maxit Number of iterations of the algorithm, by deafult set to 50.
+#' @param tolerance Tolerance for the convergence, by deafult set to NA.
+#' @param verbose Logical, set to TRUE if you used tolerance criterion and you wish to see the messages about convergence, by deafult set to FALSE.
+#' @return A list with square root and inverse square root of \code{mat}.
 #' @import assertthat
 #' @export
 #' @examples
-#' # create some numeric vectors
-#' x <- 1:10
+#' # create a matrix
+#' mat <- matrix(runif(9), 3, 3)
 #' 
-#' # min element will be transformed to 0 and max element to 1
-#' rescale(x)
-#'
-#' # arbitrary lower and upper value
-#' rescale(x, 20, 100)
+#' # taking a square root
+#' sqrtmat(mat)
 
 
-sqrtmat <- function(mat, maxit = 50) {
+sqrtmat <- function(mat, maxit = 50, tolerance = NA, verbose = FALSE) {
     # basic check of the input
-    not_empty(x)
-    assert_that(is.number(lower))
-    assert_that(is.number(upper))
-    assert_that(is.logical(na.rm))
-    assert_that(  # a numeric vector
-        is.numeric(x),
-        !is.matrix(x),
-        !is.list(x),
-        !is.data.frame(x)
-    )
-    stopifnot(nrow(mat) == ncol(mat))
-    niter <- 0
-    y <- mat
+    not_empty(mat)
+    assert_that(all(!is.na(mat)))
+    assert_that(nrow(mat) == ncol(mat))
+    assert_that(is.scalar(maxit))
+    assert_that(is.matrix(mat))
+    assert_that(is.numeric(mat))
+    assert_that(is.logical(verbose))
+    if (!is.na(tolerance)) assert_that(is.scalar(tolerance) && tolerance > 0)
+    
+    # the algorithm
     z <- diag(rep(1, nrow(mat)))
     for (niter in 1:maxit) {
-      y.temp <- 0.5*(y + solve(z))
-      z <- 0.5*(z + solve(y))
-      y <- y.temp
+        temp <- 0.5*(mat + solve(z))
+        z <- 0.5*(z + solve(mat))
+
+        # check for the convergence
+        if(!is.na(tolerance)) {
+            convergence <- all.equal(mat, temp, tolerance)
+            if(convergence) {
+                mat <- temp
+                if (verbose) print("Converged!")
+                break
+            }
+        }
+
+        # assignment, if convergence criterion not met
+        mat <- temp      
     }
-    return(list(sqrt = y, 
-                sqrt.inv = z))
+    
+    # report the mean relative difference if not converged
+    if (!is.na(tolerance) && !convergence && verbose) cat("Not converged: ", convergence, "\n")
+    return(list(sqrt = mat, 
+                sqrtinv = z))
 }
 
 
--- tests/testthat/test_math.R
+++ tests/testthat/test_math.R
@@ -18,14 +18,26 @@ context("sqrtmat function")
 
 test_that("sqrtmat - default", {
 	
-	mat <- matrix(runif(9), 3, 3)
-    mat_eig <- eigen(mat)
-	mat_sqrt <- mat_eig$vectors %*% diag(sqrt(mat_eig$values)) %*% solve(mat_eig$vectors)
+	mat <- matrix(c(3,2,1,5,7,6,4,3,2), 3, 3)
+    res <- sqrtmat(mat)
 
-    # expect_that( sum(is.na(res)), equals(d) )
+    expect_that(res$sqrt > 0, is_true() )
     # expect_that( min(res, na.rm=TRUE), equals(min(x)) )
     # expect_that( max(res, na.rm=TRUE), equals(max(x)-d) )
     # expect_that( res, is_a("integer") )
     # expect_that( length(res), is_identical_to(length(x)) )
 })
 
+test_that("sqrtmat - inputs that should fail", {
+	
+	mat <- matrix(runif(9), 3, 3)
+	mat[1,1] <- NA
+	expect_error( sqrtmat(mat) )
+	
+   
+    # expect_that( sum(is.na(res)), equals(d) )
+    # expect_that( min(res, na.rm=TRUE), equals(min(x)) )
+    # expect_that( max(res, na.rm=TRUE), equals(max(x)-d) )
+    # expect_that( res, is_a("integer") )
+    # expect_that( length(res), is_identical_to(length(x)) )
+})
